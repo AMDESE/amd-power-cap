98f8d2f25a788596b5b5c71c2541c389ea2a6042
Mohsen Dolaty <mohsen.dolaty@amd.com>
2022-04-08 10:45:03 -0500

    I3C setup is now done in power-capping application
    Signed-off-by: Mohsen Dolaty <mohsen.dolaty@amd.com>

--- src/power_cap.cpp
+++ src/power_cap.cpp
@@ -7,7 +7,7 @@
 #include <sdbusplus/asio/connection.hpp>
 #include <sdbusplus/asio/property.hpp>
 #include <gpiod.hpp>
-
+#include <filesystem>
 
 extern "C" {
 #include <unistd.h>
@@ -24,14 +24,47 @@ extern "C" {
 #define MAX_RETRY           10
 #define CPU_MAX_PWR_LIMIT   (1000) //1000 watts, max perf
 
-/* Definition for APML Mux setting */
-#define MAX_APML_BUS    0x02
+// Definition for APML Mux setting
+#define MAX_APML_BUS    2
+#define I3C_BUS_APML0   4
+#define I3C_BUS_APML1   5
+#define I3C_MUX_DEV     0x4cc00000000
+#define I3C_TSI_DEV     0x22400000001
+#define I3C_RMI_DEV     0x22400000002
 #define IMX3112_MUX     0x70
 #define IMX3112_MR46    0x46
 #define IMX3112_MR40    0x40  // MUX port sel
 #define IMX3112_MR41    0x41  // MUX port rw enable
 #define CMD_BUFF_LEN    256
-#define MAX_RETRY       0x10
+#define FNAME_LEN       64
+#define I3C_WAIT_TIME   2     // seconds
+
+
+// Platform Type
+constexpr auto ONYX_SLT     = 61;   //0x3D
+constexpr auto ONYX_1       = 64;   //0x40
+constexpr auto ONYX_2       = 65;   //0x41
+constexpr auto ONYX_3       = 66;   //0x42
+constexpr auto ONYX_FR4     = 82;   //0x52
+constexpr auto QUARTZ_DAP   = 62;   //0x3E
+constexpr auto QUARTZ_1     = 67;   //0x43
+constexpr auto QUARTZ_2     = 68;   //0x44
+constexpr auto QUARTZ_3     = 69;   //0x45
+constexpr auto QUARTZ_FR4   = 81;   //0x51
+constexpr auto RUBY_1       = 70;   //0x46
+constexpr auto RUBY_2       = 71;   //0x47
+constexpr auto RUBY_3       = 72;   //0x48
+constexpr auto TITANITE_1   = 73;   //0x49
+constexpr auto TITANITE_2   = 74;   //0x4A
+constexpr auto TITANITE_3   = 75;   //0x4B
+constexpr auto TITANITE_4   = 76;   //0x4C
+constexpr auto TITANITE_5   = 77;   //0x4D
+constexpr auto TITANITE_6   = 78;   //0x4E
+constexpr auto I3C_DRIVER_PATH = "/sys/bus/platform/drivers/dw-i3c-master/";
+constexpr auto TSI_DRIVER_PATH = "/sys/bus/i3c/drivers/sbtsi_i3c/";
+constexpr auto I3C_DEV0        = "1e7a6000.i3c4";
+constexpr auto I3C_DEV1        = "1e7a7000.i3c5";
+const     int  I3C_BUS[2] = {I3C_BUS_APML0 , I3C_BUS_APML1};
 
 const std::string PwrOkName = "MON_POST_COMPLETE";
 constexpr auto POWER_SERVICE = "xyz.openbmc_project.Settings";
@@ -45,8 +78,8 @@ constexpr auto MAPPER_INTERFACE = "xyz.openbmc_project.ObjectMapper";
 
 PowerCapDataHolder* PowerCapDataHolder::instance = 0;
 
-struct i2c_info p0_info = {4, 0x22400000002, 0};
-struct i2c_info p1_info = {5, 0x22400000002, 0};
+struct i2c_info p0_info = {I3C_BUS_APML0, I3C_RMI_DEV, 0};
+struct i2c_info p1_info = {I3C_BUS_APML1, I3C_RMI_DEV, 0};
 
 // Set power limit to CPU using OOB library
 uint32_t PowerCap::set_oob_pwr_limit (struct i2c_info bus, uint32_t req_pwr_limit)
@@ -57,14 +90,11 @@ uint32_t PowerCap::set_oob_pwr_limit (struct i2c_info bus, uint32_t req_pwr_limi
     ret = read_socket_power_limit(bus, &current_pwr_limit);
     if((ret == OOB_SUCCESS) && (current_pwr_limit != 0))
     {
-        phosphor::logging::log<phosphor::logging::level::INFO>(
-            "Initial Power Cap Value ",phosphor::logging::entry(
-            "Initial Power Cap Value %d",current_pwr_limit));
+        sd_journal_print(LOG_DEBUG, "Initial Power Cap Value %d \n",current_pwr_limit);
     }
     else
     {
-        phosphor::logging::log<phosphor::logging::level::INFO>(
-            "unable to read power limit");
+        sd_journal_print(LOG_ERR, "unable to read power limit \n");
         return -1;
     }
 
@@ -72,8 +102,7 @@ uint32_t PowerCap::set_oob_pwr_limit (struct i2c_info bus, uint32_t req_pwr_limi
      * OOB deals in milliwatts only */
     if ((current_pwr_limit/1000) == req_pwr_limit)
     {
-        phosphor::logging::log<phosphor::logging::level::INFO>(
-            "CPU already operating at requested power limit");
+        sd_journal_print(LOG_DEBUG, "CPU already operating at requested power limit \n");
         return req_pwr_limit;
     }
 
@@ -81,7 +110,7 @@ uint32_t PowerCap::set_oob_pwr_limit (struct i2c_info bus, uint32_t req_pwr_limi
     ret = write_socket_power_limit(bus, (req_pwr_limit * 1000));
     if (ret != OOB_SUCCESS)
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("Setting power cap value failed");
+        sd_journal_print(LOG_ERR, "Setting power cap value failed \n");
         return -1;
     }
 
@@ -97,7 +126,7 @@ uint32_t PowerCap::set_oob_pwr_limit (struct i2c_info bus, uint32_t req_pwr_limi
     }
     else
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("Readback power cap value failed");
+        sd_journal_print(LOG_ERR, "Readback power cap value failed \n");
         return -1;
     }
 
@@ -109,8 +138,7 @@ bool PowerCap::do_power_capping() {
 
     int ret = -1;
 
-    if((PowerCap::getPlatformID() == false) ||
-       ((userPCapLimit == 0) && (AppliedPowerCapData == 0))) /* factory defaults */
+    if ((userPCapLimit == 0) && (AppliedPowerCapData == 0)) /* factory defaults */
     {
         userPCapLimit = CPU_MAX_PWR_LIMIT; // Set very high value
                                            // CPU settles at max based on OPN
@@ -124,9 +152,7 @@ bool PowerCap::do_power_capping() {
     ret = PowerCap::set_oob_pwr_limit(p0_info, userPCapLimit);
 
     if ((ret != -1) &&         /* socket P0 set was successful */
-        ((PowerCap::BoardName.compare("Quartz") == 0) ||
-         (PowerCap::BoardName.compare("Titanite") == 0))
-       )
+	(num_of_proc == 2) )
     {
         //P1 Power Cap Value Update
         ret = PowerCap::set_oob_pwr_limit(p1_info, userPCapLimit);
@@ -151,54 +177,46 @@ bool PowerCap::getPlatformID()
 {
     FILE *pf;
     char data[COMMAND_LEN];
+    std::stringstream ss;
 
     // Setup pipe for reading and execute to get u-boot environment
     // variable board_id.
     pf = popen(COMMAND_BOARD_ID,"r");
 
-    // Error handling
-    if(pf < 0)
-    {
-        std::cerr << "Unable to get Board ID, errno: " << errno << "message: " << strerror(errno) << "\n";
-        return false;
+    if(pf > 0)
+    {   // no error
+        if (fgets(data, COMMAND_LEN , pf) != NULL)
+        {
+            ss << std::hex << (std::string)data;
+            ss >> board_id;
+        }
+        pclose(pf);
+        if ( board_id > 0 || board_id < 0xFF )
+        {
+            switch (board_id)
+            {
+                case ONYX_SLT:
+                case ONYX_1 ... ONYX_3:
+                case ONYX_FR4:
+                case RUBY_1 ... RUBY_3:
+                    num_of_proc = 1;
+                    break;
+                case QUARTZ_DAP:
+                case QUARTZ_1 ... QUARTZ_3:
+                case QUARTZ_FR4:
+                case TITANITE_1 ... TITANITE_6:
+                    num_of_proc = 2;
+                    break;
+                default:
+                    num_of_proc = 1;
+                    break;
+            }//switch
+            return true;
+        }
     }
-
-    // Get the data from the process execution
-    if (fgets(data, COMMAND_LEN, pf) == NULL)
+    else
     {
-        std::cerr << "Board ID data is null, errno: " << errno << "message: " << strerror(errno) << "\n";
-        return false;
-    }
-
-    // the data is now in 'data'
-    if (pclose(pf) != 0)
-    {
-        std::cerr << " Error: Failed to close command stream\n";
-        return false;
-    }
-    std::string board_id(data);
-    if((board_id.compare("3D") == 0) || (board_id.compare("40") == 0) || (board_id.compare("41") == 0)
-        || (board_id.compare("42") == 0) || (board_id.compare("52") == 0))
-    {
-        PowerCap::BoardName = "Onyx";
-        return true;
-    }
-    if((board_id.compare("3E") == 0 ) || (board_id.compare("43") == 0) || (board_id.compare("44") ==0)
-        || (board_id.compare("45") == 0) || (board_id.compare("51") == 0))
-    {
-        PowerCap::BoardName = "Quartz";
-        return true;
-    }
-    if((board_id.compare("46")== 0) || (board_id.compare("47") == 0) || (board_id.compare("48") == 0))
-    {
-        PowerCap::BoardName = "Ruby";
-        return true;
-    }
-    if((board_id.compare("49") == 0 ) || (board_id.compare("4A") == 0) || (board_id.compare("4B") == 0)
-        || (board_id.compare("4C") == 0) || (board_id.compare("4D") == 0) || (board_id.compare("4E") == 0))
-    {
-        PowerCap::BoardName = "Titanite";
-        return true;
+        sd_journal_print(LOG_ERR, "Failed to open command stream \n");
     }
 
     return false;
@@ -213,7 +231,7 @@ int  PowerCap::getGPIOValue(const std::string& name)
     gpioLine = gpiod::find_line(name);
     if (!gpioLine)
     {
-        std::cerr << "Can't find line: %s" << name.c_str() << std::endl;
+        sd_journal_print(LOG_ERR, "Can't find line: %s \n", name.c_str());
         return -1;
     }
     try
@@ -222,7 +240,7 @@ int  PowerCap::getGPIOValue(const std::string& name)
     }
     catch (std::system_error& exc)
     {
-        std::cerr << "Error setting gpio as Input:: %s" << name.c_str() << std::endl;
+        sd_journal_print(LOG_ERR, "Error setting gpio as Input: %s \n", name.c_str());
         return -1;
     }
 
@@ -232,31 +250,65 @@ int  PowerCap::getGPIOValue(const std::string& name)
     }
     catch (std::system_error& exc)
     {
-        std::cerr << "Error getting gpio value for: %s" << name.c_str() << std::endl;
+        sd_journal_print(LOG_ERR, "Error getting gpio value for: %s \n", name.c_str());
         return -1;
     }
 
     return value;
 }
 
-void PowerCap::enableAPMLMuxChannel()
+void system_check(char *cmd)
+{
+    if ( system(cmd) < 0)
+        sd_journal_print(LOG_ERR, "Filed to run system cmd: %s \n", cmd);
+}
+void unbindDrivers(int i)
 {
     char cmd[CMD_BUFF_LEN];
-    int num_of_apml_bus = MAX_APML_BUS;
+
+    // Unbind sbtsi driver
+    sprintf(cmd, "echo %d-%llx > %sunbind", I3C_BUS[i], I3C_TSI_DEV, TSI_DRIVER_PATH );
+    system_check(cmd);
+    sleep(I3C_WAIT_TIME);
+    // Unbind platform driver
+    if (i == 0)
+        sprintf(cmd, "echo %s > %sunbind", I3C_DEV0, I3C_DRIVER_PATH );
+    else
+        sprintf(cmd, "echo %s > %sunbind", I3C_DEV1, I3C_DRIVER_PATH );
+    system_check(cmd);
+    sleep(I3C_WAIT_TIME);
+}
+void bindDrivers(int i)
+{
+    char cmd[CMD_BUFF_LEN];
+
+    // Bind platform driver
+    if (i == 0)
+        sprintf(cmd, "echo %s > %sbind", I3C_DEV0, I3C_DRIVER_PATH );
+    else
+        sprintf(cmd, "echo %s > %sbind", I3C_DEV1, I3C_DRIVER_PATH );
+    system_check(cmd);
+    sleep(I3C_WAIT_TIME);
+}
+void setAPMLMux(int i)
+{
+    char cmd[CMD_BUFF_LEN];
+
+    // Set the Mux
+    sprintf(cmd,"/usr/bin/i3ctransfer -d /dev/i3c-%d-%llx -w 0x%x,0x00,0x01", I3C_BUS[i], I3C_MUX_DEV,  IMX3112_MR46);
+    system_check(cmd);
+    sprintf(cmd,"/usr/bin/i3ctransfer -d /dev/i3c-%d-%llx -w 0x%x,0x00,0x40", I3C_BUS[i], I3C_MUX_DEV, IMX3112_MR40);
+    system_check(cmd);
+    sprintf(cmd,"/usr/bin/i3ctransfer -d /dev/i3c-%d-%llx -w 0x%x,0x00,0x40", I3C_BUS[i], I3C_MUX_DEV, IMX3112_MR41);
+    system_check(cmd);
+
+}
+
+void PowerCap::enableAPMLMuxChannel()
+{
+    char fname[FNAME_LEN];
     int retry = 0;
     bool enableAPMLMux = false;
-
-
-    /* Code for APML bus Mux settings */
-    if ( (strcmp(PowerCap::BoardName.c_str(), "Onyx") == 0) ||
-         (strcmp(PowerCap::BoardName.c_str(), "Ruby") == 0) )
-    {
-        num_of_apml_bus = 1;
-    }
-    else
-    {
-        num_of_apml_bus = MAX_APML_BUS;
-    }
 
     while (retry < MAX_RETRY)
     {
@@ -264,24 +316,29 @@ void PowerCap::enableAPMLMuxChannel()
         sleep(10);
         if (getGPIOValue(PwrOkName) > 0)
         {
-            std::cerr <<"POST Complete reached - Enable APML Mux" << std::endl;
+            sd_journal_print(LOG_ERR, "POST Complete reached - Enable APML Mux \n");
             enableAPMLMux = true;
             break;
         }
-
-
         retry++;
     }
-
-	if (enableAPMLMux == true)
-	{
-		sleep(1);
-		sprintf(cmd, "/usr/bin/set-apml.sh>& /dev/null\n");
-		if (system(cmd) != 0)
-			std::cout <<"Failed to set APML MUX " << std::endl;
-	}
-
-    return;
+    if (enableAPMLMux == true)
+    {
+        for (int i=0; i < num_of_proc; i++ )
+        {
+	    // Unbind drivers
+            unbindDrivers(i);
+            // Bind drivers
+            bindDrivers(i);
+            // Set Mux device
+            setAPMLMux(i);
+            // Unbind drivers
+            unbindDrivers(i);
+            // Bind drivers
+            bindDrivers(i);
+        } // for loop
+    }
+    sd_journal_print(LOG_ERR, "APML MUX setting sucessful for %d CPU \n", num_of_proc);
 }
 
 // CPU loses the power limit applied after reboot
@@ -301,14 +358,13 @@ void PowerCap::onHostPwrChange()
         while((do_power_capping() == false) && (retry < MAX_RETRY))
         {
             sleep(30);
-            phosphor::logging::log<phosphor::logging::level::ERR>
-                 ("SMU not initialized, retrying...");
+            sd_journal_print(LOG_ERR, "SMU not initialized, retrying...\n");
             retry++;
         }
     }
     else
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("Power cap not enabled");
+        sd_journal_print(LOG_ERR, "Power cap not enabled \n");
     }
 
 }
@@ -336,8 +392,7 @@ void PowerCap::init_power_capping()
     }
     else if(retry >= MAX_RETRY)
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("Power cap settings not found");
-        exit -1;
+        sd_journal_print(LOG_ERR, "Power cap settings not found \n");
     }
 }
 
@@ -355,7 +410,7 @@ bool PowerCap::get_power_cap_enabled_setting()
     try
     {
         std::string settingManager = getService(bus, POWER_PATH.c_str() , POWER_INTERFACE);
-    if (settingManager.empty())
+        if (settingManager.empty())
             return false;
 
         PowerCapEnableData = getProperty<bool>(bus, settingManager.c_str(), POWER_PATH.c_str(),
@@ -363,7 +418,7 @@ bool PowerCap::get_power_cap_enabled_setting()
     }
     catch (const sdbusplus::exception::SdBusError& ex)
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("sdbus error");
+        sd_journal_print(LOG_ERR, "sdbus error \n");
     }
     return true;
 }
@@ -384,7 +439,7 @@ T PowerCap::getProperty(sdbusplus::bus::bus& bus, const char* service, const cha
     }
     catch (const sdbusplus::exception::SdBusError& ex)
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("GetProperty call failed");
+        sd_journal_print(LOG_ERR, "GetProperty call failed \n");
     }
 }
 
@@ -404,7 +459,7 @@ std::string PowerCap::getService(sdbusplus::bus::bus& bus, const char* path,
         mapperResponseMsg.read(mapperResponse);
         if (mapperResponse.empty())
         {
-            phosphor::logging::log<phosphor::logging::level::ERR>("Error reading mapper response");
+            sd_journal_print(LOG_ERR, "Error reading mapper response \n");
         }
         if (mapperResponse.size() < 1)
         {
@@ -414,7 +469,7 @@ std::string PowerCap::getService(sdbusplus::bus::bus& bus, const char* path,
     }
     catch (const sdbusplus::exception::SdBusError& ex)
     {
-        phosphor::logging::log<phosphor::logging::level::ERR>("Mapper call failed");
+        sd_journal_print(LOG_ERR, "Mapper call failed \n");
     }
     return "";
 }
@@ -430,8 +485,7 @@ void PowerCap::set_power_cap_limit(uint32_t value)
         [this](boost::system::error_code ec) {
             if (ec)
             {
-                phosphor::logging::log<phosphor::logging::level::ERR>
-                    ("Failed to set power cap value in dbus interface");
+                sd_journal_print(LOG_ERR, "Failed to set power cap value in dbus interface \n");
             }
         },
         "xyz.openbmc_project.Settings",
